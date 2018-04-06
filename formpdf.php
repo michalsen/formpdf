c0f46ae00fd50c10dea53024bf278606af00ad24
Eric Michalsen <eric.michalsen@gmail.com>
2018-04-06 15:44:46 -0500

    printing pdf as reference for type

--- formpdf.php
+++ formpdf.php
@@ -29,3 +29,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 if(admin){
   include_once( 'includes/admin/class_formpdf_admin.php' );
 }
+
+add_filter('template_redirect', 'formpdf_override' );
+
+function formpdf_override() {
+  global $wp_query;
+
+  echo  the_field('rm_file');
+}
+
+
+add_action('formpdf', 'formPDF_trial1', 407);
+
+function formPDF_trial1() {
+    //echo  'trial: ' . the_field('rm_file');
+    return;
+}
